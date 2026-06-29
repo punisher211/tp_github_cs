b816feecd747da7665d8c0628cffdb66ffb06776
punisher211 <fayoub252@gmail.com>
2026-06-29 11:49:14 +0100

    prblm6

--- login.php
+++ login.php
@@ -18,7 +18,7 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
     $mdp_saisi=$_POST['password'];
     $requete="SELECT * FROM utilisateurs WHERE nom_utilisateur='$user_saisi' AND mot_de_passe='$mdp_saisi'";
     $resultat=$conn->query($requete);
-    if($resultat->nm_rows>0){
+    if($resultat->num_rows>0){
         $utilisateur=$resultat->fetch_assoc();
         echo"<h3 style='color:green;'> Accès Autorisé ! Bienvenue ".$utilisateur['nom_utilisateur']."</h3>";
     }else{
